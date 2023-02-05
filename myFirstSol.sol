079aa1f6d33d26ea022fbffc083d9aa8ecf757cf
TomJegou <tomjegou10@gmail.com>
2023-02-05 23:58:36 +0100

    my first commit

---
+++ myFirstSol.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.10;
+
+contract HelloWorld{
+    string public greet = "Hello World";
+}
+
+contract Varibales{
+    uint8 public u8 = 10;
+    uint256 public u256 = 100;
+    uint256 public u = 700;
+    int public i = -123;
+    address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+}
